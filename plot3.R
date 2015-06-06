5c73e5e7f481bc77d7f690051b3919921b618360
Pixel81 <pat201281@gmail.com>
2015-06-06 22:06:06 +0200

    Plot 3 update function

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-## Plotting script 2
+## Plotting script 3
 ## Written by Pixel
 ## Last update : 2015.6.6
 
@@ -25,24 +25,27 @@ data <- read.csv.sql( "./data/household_power_consumption.txt",
                                 ("1/2/2007","2/2/2007") '
                      )
 
-# 3. Draw 3 sub metering graphs
-dateTime   <- as.POSIXlt(paste(as.Date(data$Date, format="%d/%m/%Y"), 
-                               data$Time, sep=" ")
+# 3. Draw 3 sub metering graphs in plot3.png
+dateTime   <- as.POSIXlt(paste(as.Date(data$Date, format = "%d/%m/%Y"), 
+                               data$Time, sep = " ")
                          )
-plot(dateTime, data$Sub_metering_1,type="l",
-     xlab="",ylab="Energy sub metering",
-     width=480, height=480)
-points(dateTime, data$Sub_metering_3,type="l",col="blue")
-points(dateTime, data$Sub_metering_2,type="l",col="red")
 
-legend("topright",
-       lty=1, # FOr line graph
-       col = c("black","blue","red"), 
+png("plot3.png", width = 480, height = 480) # Create PNG file
+
+plot(dateTime, data$Sub_metering_1,type = "l",
+     xlab = "",ylab = "Energy sub metering") # Line 1
+points(dateTime, data$Sub_metering_3,type = "l",col = "blue") # Add second line
+points(dateTime, data$Sub_metering_2,type = "l",col = "red") # Add third line
+
+legend("topright", # Add legend
+       lty = 1, # For line graph
+       col = c("black", # Line 1 color legend
+               "blue", # Line 2 color legend
+               "red"), # Line 3 color legend
        legend = c("Sub_metering_1",
                   "Sub_metering_2",
                   "Sub_metering_3")
        )
 
-# 4. Copy histogram into PNG file in the working directory
-dev.copy(png, file = "plot3.png")
+# 4. Close connexion of PNG file
 dev.off()
